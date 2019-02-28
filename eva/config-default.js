2f4bb2d847fd34d93ba95768ffa2ba20ac34e768
Cristiano Pimenta <cgpimenta@dcc.ufmg.br>
2019-02-28 13:15:07 -0300

    Removed unused files

--- eva/config-default.js
+++
@@ -1,5 +0,0 @@
-var evaConfig = {
-    apiUrl: 'http://localhost:4224',
-    logCheckingInterval: 1000,
-    jobsCheckingInterval: 1000
-};
--- results/_settings.plt
+++
@@ -1,6 +0,0 @@
-window = '2000'
-
-allop='allOperators'
-xover='basicTypedXover'
-mut_1='oneParamMutation'
-mut_2='sameSizeSubtreeMutation'
